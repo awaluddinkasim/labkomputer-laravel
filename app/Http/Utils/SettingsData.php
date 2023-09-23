9581e565830c8a40618415293806e391a1c94127
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2023-09-23 18:51:03 +0800

    app data exception

--- app/Http/Utils/SettingsData.php
+++ app/Http/Utils/SettingsData.php
@@ -3,18 +3,23 @@
 namespace App\Http\Utils;
 
 use App\Models\Setting;
+use Illuminate\Database\QueryException;
 
 class SettingsData {
     public static function get()
     {
-        $data = [
-            'semester' => Setting::where('name', 'semester')->first(),
-            'upload' => Setting::where('name', 'upload')->first(),
+        try {
+            $data = [
+                'semester' => Setting::where('name', 'semester')->first(),
+                'upload' => Setting::where('name', 'upload')->first(),
 
-            'kepala_lab' => Setting::where('name', 'kepala_lab')->first(),
-            'asisten1' => Setting::where('name', 'asisten1')->first(),
-            'asisten2' => Setting::where('name', 'asisten2')->first(),
-        ];
+                'kepala_lab' => Setting::where('name', 'kepala_lab')->first(),
+                'asisten1' => Setting::where('name', 'asisten1')->first(),
+                'asisten2' => Setting::where('name', 'asisten2')->first(),
+            ];
+        } catch (QueryException $e) {
+            $data = [];
+        }
 
         return $data;
     }
