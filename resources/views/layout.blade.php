c351e45d16156e57f44416a44f639ab23b88f28a
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2023-09-24 08:39:58 +0800

    fix settings modal for admin only

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -47,88 +47,94 @@
         </div>
     </div>
 
-    <div class="modal fade" id="settings" tabindex="-1" aria-labelledby="settingsLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="settingsLabel">Pengaturan</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
+    @auth('admin')
+        <div class="modal fade" id="settings" tabindex="-1" aria-labelledby="settingsLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="settingsLabel">Pengaturan</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <form action="{{ route('admin.pengaturan-save') }}" method="POST">
+                        @csrf
+                        <div class="modal-body">
+                            <div class="card-title">
+                                <h6>Pengaturan sistem</h6>
+                            </div>
+                            <div class="form-group row">
+                                <label for="semester" class="col-sm-6 col-form-label align-self-center">Semester</label>
+                                <div class="col-sm-6">
+                                    <select class="custom-select mr-sm-2" id="semester" name="semester" required>
+                                        <option selected hidden value="">Pilih</option>
+                                        <option value="ganjil"
+                                            {{ $settings['semester']['value'] == 'ganjil' ? 'selected' : '' }}>
+                                            Ganjil</option>
+                                        <option value="genap"
+                                            {{ $settings['semester']['value'] == 'genap' ? 'selected' : '' }}>
+                                            Genap</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="upload" class="col-sm-6 col-form-label align-self-center">Opsi Upload
+                                    Slip</label>
+                                <div class="col-sm-6">
+                                    <select class="custom-select mr-sm-2" id="upload" name="upload" required>
+                                        <option selected hidden value="">Pilih</option>
+                                        <option value="open"
+                                            {{ $settings['upload']['value'] == 'open' ? 'selected' : '' }}>
+                                            Upload Terbuka</option>
+                                        <option value="closed"
+                                            {{ $settings['upload']['value'] == 'closed' ? 'selected' : '' }}>
+                                            Upload Tertutup</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="card-title">
+                                <h6>Kontak</h6>
+                            </div>
+                            <div class="form-group">
+                                <label for="kepala_lab">Kepala Lab</label>
+                                <div class="input-group mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text">+62</span>
+                                    </div>
+                                    <input type="text" class="form-control" id="kepala_lab" name="kepala_lab"
+                                        autocomplete="off" value="{{ $settings['kepala_lab']['value'] }}" required>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="asisten1">Asisten Lab 1</label>
+                                <div class="input-group mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text">+62</span>
+                                    </div>
+                                    <input type="text" class="form-control" id="asisten1" name="asisten1"
+                                        autocomplete="off" value="{{ $settings['asisten1']['value'] }}" required>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="asisten2">Asisten Lab 2</label>
+                                <div class="input-group mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text">+62</span>
+                                    </div>
+                                    <input type="text" class="form-control" id="asisten2" name="asisten2"
+                                        autocomplete="off" value="{{ $settings['asisten2']['value'] }}" required>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+                            <button type="submit" class="btn btn-success">Simpan</button>
+                        </div>
+                    </form>
                 </div>
-                <form action="{{ route('admin.pengaturan-save') }}" method="POST">
-                    @csrf
-                    <div class="modal-body">
-                        <div class="card-title">
-                            <h6>Pengaturan sistem</h6>
-                        </div>
-                        <div class="form-group row">
-                            <label for="semester" class="col-sm-6 col-form-label align-self-center">Semester</label>
-                            <div class="col-sm-6">
-                                <select class="custom-select mr-sm-2" id="semester" name="semester" required>
-                                    <option selected hidden value="">Pilih</option>
-                                    <option value="ganjil" {{ $settings['semester']['value'] == 'ganjil' ? 'selected' : '' }}>
-                                        Ganjil</option>
-                                    <option value="genap" {{ $settings['semester']['value'] == 'genap' ? 'selected' : '' }}>
-                                        Genap</option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label for="upload" class="col-sm-6 col-form-label align-self-center">Opsi Upload
-                                Slip</label>
-                            <div class="col-sm-6">
-                                <select class="custom-select mr-sm-2" id="upload" name="upload" required>
-                                    <option selected hidden value="">Pilih</option>
-                                    <option value="open" {{ $settings['upload']['value'] == 'open' ? 'selected' : '' }}>
-                                        Upload Terbuka</option>
-                                    <option value="closed" {{ $settings['upload']['value'] == 'closed' ? 'selected' : '' }}>
-                                        Upload Tertutup</option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="card-title">
-                            <h6>Kontak</h6>
-                        </div>
-                        <div class="form-group">
-                            <label for="kepala_lab">Kepala Lab</label>
-                            <div class="input-group mb-3">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text">+62</span>
-                                </div>
-                                <input type="text" class="form-control" id="kepala_lab" name="kepala_lab"
-                                    autocomplete="off" value="{{ $settings['kepala_lab']['value'] }}" required>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="asisten1">Asisten Lab 1</label>
-                            <div class="input-group mb-3">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text">+62</span>
-                                </div>
-                                <input type="text" class="form-control" id="asisten1" name="asisten1"
-                                    autocomplete="off" value="{{ $settings['asisten1']['value'] }}" required>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="asisten2">Asisten Lab 2</label>
-                            <div class="input-group mb-3">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text">+62</span>
-                                </div>
-                                <input type="text" class="form-control" id="asisten2" name="asisten2"
-                                    autocomplete="off" value="{{ $settings['asisten2']['value'] }}" required>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
-                        <button type="submit" class="btn btn-success">Simpan</button>
-                    </div>
-                </form>
             </div>
         </div>
-    </div>
+    @endauth
     @stack('modals')
 
     @include('includes.scripts')
