f9bd59ef52c9710843458f892deb186459e2b7ad
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2023-10-15 19:10:27 +0800

    pindahkan pusher script

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -139,26 +139,8 @@
 
     @include('includes.scripts')
 
-    @if (Auth::guard('admin')->check() || Auth::user()->level == 'asisten')
-        <script src="https://js.pusher.com/7.2/pusher.min.js"></script>
-        <script>
-            var pusher = new Pusher('02a61b9baa791a2610a5', {
-                cluster: 'ap1'
-            });
+    @include('includes.pusher')
 
-            var channel = pusher.subscribe('account');
-            channel.bind('user-registered', function(data) {
-                if ($('#unverified').length > 0) {
-                    $('#unverified').text(data.totalUnverified);
-                } else {
-                    $('.mhs').html('Mahasiswa <span id="unverified" class="badge badge-danger ml-auto">' + data
-                        .totalUnverified + '</span>');
-                    $('.daftar-mhs').html('Daftar Mahasiswa <span id="unverified" class="badge badge-danger ml-auto">' +
-                        data.totalUnverified + '</span>');
-                }
-            });
-        </script>
-    @endif
     @if (Auth::guard('admin')->check())
         @if (Session::has('settings-saved'))
             <script>
